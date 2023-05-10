33365b1759894c8133a8186432d676219251c1c0
sebastien <heligon.seb@gmail.com>
2023-05-10 11:03:33 +0200

    add route infos user / create user #7

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,8 +7,10 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
@@ -22,6 +24,14 @@ class UserController extends AbstractController
         return new JsonResponse($jsonUsersList, Response::HTTP_OK, [], true);
     }
 
+    #[Route('/api/users/{id}', name: 'infos_user', methods: ['GET'])]
+    public function getInfosUser(User $user, SerializerInterface $serializer): JsonResponse
+    {
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+
+        return new JsonResponse($jsonUser, Response::HTTP_OK, [], true);
+    }
+
     #[Route('/api/users/{id}', name: 'delete_user', methods: ['DELETE'])]
     public function deleteUser(User $user, EntityManagerInterface $entityManager): JsonResponse
     {
@@ -31,6 +41,19 @@ class UserController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
-    // #[Route('/api/users/add', name: 'add_user')]
-    // public function addUser(): JsonResponse
+    #[Route('/api/users', name: 'add_user', methods: ['POST'])]
+    public function createUser(Request $request, SerializerInterface $serializer, EntityManagerInterface $entityManager, UrlGeneratorInterface $urlGenerator): JsonResponse
+    {
+        $user = $serializer->deserialize($request->getContent(), User::class, 'json');
+        $user->setClient($this->getUser());
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        $jsonUser = $serializer->serialize($user, 'json', ['groups' => 'getUsers']);
+
+        $location = $urlGenerator->generate('infos_user', ['id' => $user->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUser, Response::HTTP_CREATED, ["location" => $location], true);
+    }
 }
