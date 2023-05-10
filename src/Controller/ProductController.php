5eccd001573a963f2299d97023df185518192f89
sebastien <heligon.seb@gmail.com>
2023-05-10 10:42:15 +0200

    add delete user route

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -13,31 +13,20 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ProductController extends AbstractController
 {
     #[Route('/api/products', name: 'products', methods: ['GET'])]
-    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializerInterface): JsonResponse
+    public function getAllProducts(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $productsList = $productRepository->findAll();
 
-        $jsonProductsList = $serializerInterface->serialize($productsList, 'json');
+        $jsonProductsList = $serializer->serialize($productsList, 'json');
 
         return new JsonResponse($jsonProductsList, Response::HTTP_OK, [], true);
     }
 
     #[Route('/api/products/{id}', name: 'product', methods: ['GET'])]
-    public function getProduct(Product $product, SerializerInterface $serializerInterface): JsonResponse
+    public function getProduct(Product $product, SerializerInterface $serializer): JsonResponse
     {
-        $jsonProduct = $serializerInterface->serialize($product, 'json');
+        $jsonProduct = $serializer->serialize($product, 'json');
 
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
-
-
-        // $product = $productRepository->find($id);
-
-        // if ($product) {
-        //     $jsonProduct = $serializerInterface->serialize($product, 'json');
-
-        //     return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
-        // }
-
-        // return new JsonResponse(null, Response::HTTP_NOT_FOUND);
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,12 +13,24 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class UserController extends AbstractController
 {
-    #[Route('/api/users', name: 'client_users')]
-    public function getUserFromClient(UserRepository $userRepository, SerializerInterface $serializerInterface): JsonResponse
+    #[Route('/api/users', name: 'client_users', methods: ['GET'])]
+    public function getUserFromClient(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
     { 
         $usersList = $userRepository->findby(['Client' => $this->getUser()]);
 
-        $jsonUsersList = $serializerInterface->serialize($usersList, 'json', ['groups' => 'getUsers']);
+        $jsonUsersList = $serializer->serialize($usersList, 'json', ['groups' => 'getUsers']);
         return new JsonResponse($jsonUsersList, Response::HTTP_OK, [], true);
     }
+
+    #[Route('/api/users/{id}', name: 'delete_user', methods: ['DELETE'])]
+    public function deleteUser(User $user, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $entityManager->remove($user);
+        $entityManager->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    // #[Route('/api/users/add', name: 'add_user')]
+    // public function addUser(): JsonResponse
 }
