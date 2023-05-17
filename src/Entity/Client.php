dd0363f7db89f919be76e578382556870df83141
sebastien <heligon.seb@gmail.com>
2023-05-17 14:53:05 +0200

    correction symfony insight

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -114,7 +114,7 @@ class Client implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
         return;
     }
